fe5e50c8f77b33a9a1622352cad02f891a01524e
NO_ob <15161159+NO-ob@users.noreply.github.com>
2020-01-09 13:05:53 +0000

    Get Number From FileName

--- pspVidConv.sh
+++ pspVidConv.sh
@@ -1,14 +1,13 @@
 echo "Gib Output FileName"
 read outName
-ep=1
 mkdir "$outName"-PSPReady
 mkdir tmp
 for i in *.mkv
 do
-  ffmpeg  -i "$i" tmp/"$outName"_`printf "%02d" $ep`-PSPReady.ass
-  ffmpeg -y -i "$i" -flags +bitexact -vcodec libx264 -profile:v baseline -level 3.0 -s 480x272 -b:v 1m -acodec aac -b:a 128k -ar 48000 -strict -2 -filter:v subtitles=tmp/"$outName"_`printf "%02d" $ep`-PSPReady.ass:force_style='Fontsize=46' "$outName"-PSPReady/"$outName"_`printf "%02d" $ep`-PSPReady.MP4
-  ffmpeg -i "$i" -f image2 -ss 5 -vframes 1 -s 160x120  "$outName"-PSPReady/"$outName"_`printf "%02d" $ep`-PSPReady.THM
-  ((ep ++))
+  epNum=`echo $i| egrep -o "[0-9]+" | head -1`
+  ffmpeg  -i "$i" tmp/"$outName"_$epNum-PSPReady.ass
+  ffmpeg -y -i "$i" -flags +bitexact -vcodec libx264 -profile:v baseline -level 3.0 -s 480x272 -b:v 1m -acodec aac -b:a 128k -ar 48000 -strict -2 -filter:v subtitles=tmp/"$outName"_$epNum-PSPReady.ass:force_style='Fontsize=46' "$outName"-PSPReady/"$outName"_$epNum-PSPReady.MP4
+  ffmpeg -i "$i" -f image2 -ss 5 -vframes 1 -s 160x120  "$outName"-PSPReady/"$outName"_$epNum-PSPReady.THM
 done
 
 rm -rf tmp
